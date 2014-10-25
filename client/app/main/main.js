356e5031609447501326e970337f6ffa66992652
jorgep19 <jorgelpaez19@gmail.com>
2014-10-24 22:27:28 -0400

    init

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('munchApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
